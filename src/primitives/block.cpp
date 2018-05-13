4095557e3b03ff4b2311d4e480ebaed8e03eb8da
Uhlik <uhlik@uhlik.sk>
2018-05-13 03:16:11 +0200

    nist5 algo implemented
    (cherry picked from commit 200478b09f4b72edfcd8295a2e10dd0c123ac7be)

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -10,6 +10,7 @@
 #include <crypto/common.h>
 
 // FXTC BEGIN
+#include <crypto/nist5.h>
 #include <crypto/scrypt.h>
 // FXTC END
 
@@ -29,6 +30,7 @@ uint256 CBlockHeader::GetPoWHash() const
     {
         case ALGO_SHA256D: powHash = GetHash(); break;
         case ALGO_SCRYPT:  scrypt_1024_1_1_256(BEGIN(nVersion), BEGIN(powHash)); break;
+        case ALGO_NIST5:   powHash = NIST5(BEGIN(nVersion), END(nNonce)); break;
         default:           break; // FXTC TODO: we should not be here
     }
 
