bd73a681374589611676376198dc4f6717c4a9cc
Uhlik <uhlik@uhlik.sk>
2018-06-14 02:58:29 +0200

    network hash calculated by mining algorithm
    (cherry picked from commit 69531033facecf9b3584a2cf051aa024218a2e3b)

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -47,7 +47,7 @@
  * or from the last difficulty change if 'lookup' is nonpositive.
  * If 'height' is nonnegative, compute the estimate at the time when a given block was found.
  */
-static UniValue GetNetworkHashPS(int lookup, int height) {
+static UniValue GetNetworkHashPS(int lookup, int height, int32_t nAlgo) {
     CBlockIndex *pb = ::ChainActive().Tip();
 
     if (height >= 0 && height < ::ChainActive().Height())
@@ -79,6 +79,26 @@ static UniValue GetNetworkHashPS(int lookup, int height) {
         return 0;
 
     arith_uint256 workDiff = pb->nChainWork - pb0->nChainWork;
+    // FXTC BEGIN
+    switch (nAlgo)
+    {
+        case ALGO_SHA256D:
+            workDiff = pb->nChainWorkSha256d - pb0->nChainWorkSha256d;
+            break;
+        case ALGO_SCRYPT:
+            workDiff = pb->nChainWorkScrypt - pb0->nChainWorkScrypt;
+            break;
+        case ALGO_NIST5:
+            workDiff = pb->nChainWorkNist5 - pb0->nChainWorkNist5;
+            break;
+        case ALGO_LYRA2Z:
+            workDiff = pb->nChainWorkLyra2Z - pb0->nChainWorkLyra2Z;
+            break;
+        case ALGO_X11:
+            workDiff = pb->nChainWorkX11 - pb0->nChainWorkX11;
+            break;
+    }
+    // FXTC END
     int64_t timeDiff = maxTime - minTime;
 
     return workDiff.getdouble() / timeDiff;
@@ -93,6 +113,7 @@ static UniValue getnetworkhashps(const JSONRPCRequest& request)
                 {
                     {"nblocks", RPCArg::Type::NUM, /* default */ "120", "The number of blocks, or -1 for blocks since last difficulty change."},
                     {"height", RPCArg::Type::NUM, /* default */ "-1", "To estimate at the time of the given height."},
+                    {"algorithm", RPCArg::Type::STR, /* default */ "", "Filter work for selected algorithm.."},
                 },
                 RPCResult{
             "x             (numeric) Hashes per second estimated\n"
@@ -104,7 +125,7 @@ static UniValue getnetworkhashps(const JSONRPCRequest& request)
             }.Check(request);
 
     LOCK(cs_main);
-    return GetNetworkHashPS(!request.params[0].isNull() ? request.params[0].get_int() : 120, !request.params[1].isNull() ? request.params[1].get_int() : -1);
+    return GetNetworkHashPS(!request.params[0].isNull() ? request.params[0].get_int() : 120, !request.params[1].isNull() ? request.params[1].get_int() : -1, !request.params[2].isNull() ? GetAlgoId(request.params[2].get_str()) : miningAlgo);
 }
 
 static UniValue generateBlocks(const CScript& coinbase_script, int nGenerate, uint64_t nMaxTries)
